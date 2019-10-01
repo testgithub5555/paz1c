cd438e0e4a90ee165d955e854661ce9f2eda72e4
User <User@DESKTOP-97QT8TM>
2019-10-01 09:18:43 +0200

    first commit

---
+++ src/main/java/upjs_paz/paz1c_cvicenie/App.java
@@ -0,0 +1,13 @@
+package upjs_paz.paz1c_cvicenie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" ); 
+    }
+}
